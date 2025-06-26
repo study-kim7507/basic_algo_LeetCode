e6eee5cb483f11556585207b879786022b300162
study-kim7507 <study.kim7507@gmail.com>
2025-06-26 16:47:17 +0900

    Time: 0 ms (100%), Space: 7.7 MB (79.59%) - LeetHub

---
+++ 1307-ugly-number-iii/1307-ugly-number-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int nthUglyNumber(int n, long long a, long long b, long long c) {
+        long long l = 0;
+        long long r = 2e9;
+
+        int answer = 0;
+        while (l <= r)
+        {
+            long long mid = l + (r - l) / 2;
+            int cnt = 
+            mid / a + mid / b + mid / c 
+            - mid / (a * b / __gcd(a, b)) 
+            - mid / (a * c / __gcd(a, c))
+            - mid / (b * c / __gcd(b, c))
+            + mid / lcm(a, lcm(b, c));
+            if (cnt >= n)
+            {
+                answer = mid;
+                r = mid - 1;
+            }
+            else l = mid + 1;
+        }
+
+        return answer;
+    }
+};
