61d0d8e3c6abc5e1505f72ea55e67370b2fca38c
study-kim7507 <study.kim7507@gmail.com>
2025-09-01 16:07:53 +0900

    Time: 56 ms (87.93%), Space: 126.4 MB (80.47%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        map<int, int> dayMap;
-        int prefixSum = 0, freeDays = 0, previousDay = days;
+        int freeDays = 0, latestEnd = 0;
 
-        for (auto& meeting : meetings) {
-            previousDay = min(previousDay, meeting[0]); // 첫 회의가 시작하는 날을 알기 위해
-            dayMap[meeting[0]]++;   // 회의가 시작하는 날에는 +1
-            dayMap[meeting[1] + 1]--;   // 회의가 끝나는 날 +1 에는 -1
+        // 회의 시작일 기준으로 오름차순 정렬
+        sort(meetings.begin(), meetings.end());
+
+        for (auto& meeting : meetings)
+        {
+            int start = meeting[0], end = meeting[1];
+
+            if (start > latestEnd + 1)
+                freeDays += start - latestEnd - 1;
+            latestEnd = max(latestEnd, end);
         }
 
-        freeDays += (previousDay - 1); // 첫 회의가 시작하는 날 전에는 모두 free한 날임
-        for (auto& day : dayMap) {
-            int currentDay = day.first;
-            
-            if (prefixSum == 0) {   // 현재까지 진행 중이 회의 수 0이면 아무런 회의가 진행 X, 0 보다 크면 회의가 진행 중
-                freeDays += currentDay - previousDay;   
-            }
-            prefixSum += day.second;
-            previousDay = currentDay;
-        }
+        freeDays += days - latestEnd;
 
-        // Add all days after the last day of meetings
-        freeDays += days - previousDay + 1;
         return freeDays;
     }
 };
