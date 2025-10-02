c75c82b648a7163c3986e8f56ae497b8a3b92c19
study-kim7507 <study.kim7507@gmail.com>
2025-10-02 16:22:06 +0900

    Time: 5 ms (21.95%), Space: 8.5 MB (77.56%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.cpp
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {        
-        int numFullBottles = numBottles;
         int numEmptyBottles = 0;
         int numBottlesDrunk = 0;
 
         // 최초 현재 가진 물을 다 마심
-        numEmptyBottles = numBottlesDrunk = numFullBottles;
-        numFullBottles = 0;
-
+        numEmptyBottles = numBottlesDrunk = numBottles;
+        
         while(numEmptyBottles >= numExchange)
         {
-            while(numEmptyBottles >= numExchange)
-            {
-                numEmptyBottles -= numExchange;
-                numFullBottles++;
-                numExchange++;   
-            }
-            numBottlesDrunk += numFullBottles;
-            numEmptyBottles += numFullBottles;
-            numFullBottles = 0;
+            numEmptyBottles -= numExchange;
+            numBottlesDrunk++;
+            numEmptyBottles++;
+            numExchange++;
         }
         
         return numBottlesDrunk;
