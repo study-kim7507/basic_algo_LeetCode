11abf193d86a5afdd8f4e89e8a4c2e859dec0466
study-kim7507 <study.kim7507@gmail.com>
2025-10-13 14:42:41 +0900

    Time: 0 ms (100%), Space: 16.7 MB (37.57%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -2,31 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s1, string s2)
     {
-        unordered_map<char, int> um_s1;
-        unordered_map<char, int> um_s2;
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
 
-        for (auto e : s1)
-            um_s1[e]++;
-        for (auto e : s2)
-            um_s2[e]++;
-
-        for (auto it = um_s1.begin(); it != um_s1.end(); it++)
-        {
-            char curChar = it->first;
-            int curCharCount = it->second;
-
-            if (um_s2.find(curChar) == um_s2.end())
-                return false;
-            if (um_s2.find(curChar) != um_s2.end() && um_s2[curChar] != curCharCount)    
-                return false;
-            
-            um_s2[curChar] = 0;
-        }
-
-        for (auto it = um_s2.begin(); it != um_s2.end(); it++)
-            if (it->second != 0) return false;
-
-        return true;
+        return s1 == s2;
     }
 
     vector<string> removeAnagrams(vector<string>& words) {
