db63081e4befa0d1e940b98566f2e105b6f814d8
study-kim7507 <study.kim7507@gmail.com>
2025-10-16 14:56:51 +0900

    Time: 4 ms (85.48%), Space: 120.7 MB (63.8%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        int ans = 0;
-
         vector<int> candi(value, 0);
         for (auto num : nums)
-        {
-            int q = abs(num) / value;
-            int m = abs(num) % value;
+            candi[((num % value) + value) % value]++;
 
-            if (num > 0) candi[m]++;
-            else
-            {
-                if (m == 0) candi[m]++;
-                else candi[value - m]++;
-            }
-        }
-
-        int minQ = *min_element(candi.begin(), candi.end());
-        for (int i = 0; i < value; i++)
-        {
-            if (candi[i] - minQ == 0)
-            {
-                ans = value * minQ + i;
-                break;
-            }
-        }
-
+        int ans = 0;
+        while (candi[ans % value]--) ans++;
         return ans;
     }
 };
