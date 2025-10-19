304d0238931368ee941f42755e4b5605983459f9
study-kim7507 <study.kim7507@gmail.com>
2025-10-19 15:46:04 +0900

    Time: 208 ms (68.14%), Space: 103.2 MB (28.76%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    string ans = "";
-    unordered_set<string> visit;
-
-    void dfs(string curString, int a, int b)
-    {
-        if (visit.find(curString) != visit.end())
-        {
-            if (ans.empty()) ans = curString;
-            else ans = min(ans, curString);
-
-            return;
-        }
-
-        visit.insert(curString);
-        
-        // operation : add
-        string newString1 = curString;
-        for (int i = 1; i < newString1.size(); i += 2)
-        {
-            int target = newString1[i] - '0';
-            target += a;
-            if (target >= 10) target -= 10;
-            newString1[i] = target + '0';
-        }
-        dfs(newString1, a, b);
-
-        // operation : rotate
-        string prefix = curString.substr(0, b);
-        string postfix = curString.substr(b);
-        string newString2 = postfix + prefix;
-
-        dfs(newString2, a, b);
-    }
-
     string findLexSmallestString(string s, int a, int b) {
-        dfs(s, a, b);
+        int sLength = s.size();
+        string ans(sLength, '9');
+
+        unordered_set<string> visit;
+        queue<string> q;
+
+        visit.insert(s);
+        q.push(s);
+
+        while (!q.empty())
+        {
+            string cur = q.front(); q.pop();
+            ans = min(ans, cur);
+
+            // operation : add
+            string nxt1 = cur;
+            for (int i = 1; i < sLength; i += 2)
+                nxt1[i] = ((cur[i] - '0' + a) % 10) + '0';
+
+            // operation : rotate
+            string nxt2 = cur.substr(b) + cur.substr(0, b);
+            
+            if (!visit.count(nxt1)) 
+            {
+                visit.insert(nxt1);
+                q.push(nxt1);
+            }
+            if (!visit.count(nxt2))
+            {
+                visit.insert(nxt2);
+                q.push(nxt2);
+            } 
+        }        
 
         return ans;
     }
