7acf962819d5e8bde6659e79be0df711ea3e76e8
study-kim7507 <study.kim7507@gmail.com>
2025-10-29 09:09:04 +0900

    Time: 0 ms (100%), Space: 8.1 MB (99.74%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -3,10 +3,7 @@ public:
     int smallestNumber(int n) {
         int ans = 1;
         while (ans < n)
-        {
             ans |= ans << 1;
-            std::cout << ans << " ";
-        }
         return ans;
     }
 };
