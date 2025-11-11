f25a2da2cb4baba07ab3cdc84ea8fb6fac2a2744
study-kim7507 <study.kim7507@gmail.com>
2025-11-11 14:28:27 +0900

    Time: 26 ms (98.6%), Space: 12.8 MB (99.17%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int dp[601][101][101];
-
-    int solve(int i, int curZeroes, int curOnes, vector<pair<int, int>>& cost, int m, int n)
-    {
-        if (curZeroes > m || curOnes > n) return -1;
-        if (i == cost.size()) return 0;
-
-        if (dp[i][curZeroes][curOnes] != 0) return dp[i][curZeroes][curOnes];
-
-        int take = 1 + solve(i + 1, curZeroes + cost[i].first, curOnes + cost[i].second, cost, m, n);
-        int skip = solve(i + 1, curZeroes, curOnes, cost, m, n);
-
-        return dp[i][curZeroes][curOnes] = max(take, skip);
-    }
-
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<pair<int, int>> cost;
+        int dp[101][101];
+        memset(dp, 0, sizeof(dp));
+
         for (auto& str : strs)
         {
-            int zero = 0, one = 0;
+            int c0 = 0, c1 = 0;
             for (int i = 0; i < str.size(); i++)
             {
-                if (str[i] == '0') zero++;
-                else if (str[i] == '1') one++;
+                if (str[i] == '0') c0++;
+                else if (str[i] == '1') c1++;
             }
-            cost.push_back({zero, one});
+            for (int i = m; i >= c0; i--)
+            {
+                for (int j = n; j >= c1; j--)
+                    dp[i][j] = max(dp[i][j], dp[i - c0][j - c1] + 1);
+            }
         }
 
-        return solve(0, 0, 0, cost, m, n);
+        return dp[m][n];
     }
 };
