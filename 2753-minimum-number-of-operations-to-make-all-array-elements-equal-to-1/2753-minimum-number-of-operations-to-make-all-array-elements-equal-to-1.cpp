9a90b943784c528a00143e000599452facc5db6c
study-kim7507 <study.kim7507@gmail.com>
2025-11-12 13:39:37 +0900

    Time: 0 ms (100%), Space: 31.8 MB (34.03%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -3,11 +3,14 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         int oneCnt = 0;
+
+        // 1. 주어진 배열 내에 하나라도 1이 있으면, 해당 1이 퍼지면서 모든 수를 1로 만들 수 있음
         for (auto& num : nums)
             if (num == 1) oneCnt++;
         if (oneCnt != 0)
             return n - oneCnt;
 
+        // 2. 그렇지 않다면 
         int ans = INT_MAX;
         for (int i = 0; i < n; i++)
         {
@@ -17,12 +20,12 @@ public:
                 gcd = __gcd(gcd, nums[j]);
                 if (gcd == 1)
                 {
-                    ans = min(ans, j - i + n - 1);
+                    ans = min(ans, j - i);
                     break;
                 }
             }
         }   
 
-        return ans == INT_MAX ? -1 : ans;
+        return ans == INT_MAX ? -1 : ans + n - 1;
     }
 };
